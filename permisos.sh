b6b5a8e8a3d8244b9008db1f0701115d2ed8f8b9
Camilo Alarcon <cialarco@alumnos.ubiobio.cl>
2014-06-05 01:29:08 -0400

    script para renovar permisos en /protected/runtime y /assets

---
+++ permisos.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+# -*- ENCODING: UTF-8 -*-
+
+echo "<° Linux es lo mejor"
+echo "sudo chmod -R 777 /srv/http/yii/sgpp/protected/runtime"
+sudo chmod -R 777 /srv/http/yii/sgpp/protected/runtime
+sudo chmod -R 777 /srv/http/yii/sgpp/assets
+echo "sudo chmod -R 777 /srv/http/yii/sgpp/assets"
+exit
